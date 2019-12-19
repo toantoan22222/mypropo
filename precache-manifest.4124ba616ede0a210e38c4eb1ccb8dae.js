026701dcb6a4ed67cee86ddbdff1c3da6713d826
toantoan222 <toan.phan2501workplace@gmail.com>
2019-12-19 10:12:39 +0700

    Updates

---
+++ precache-manifest.4124ba616ede0a210e38c4eb1ccb8dae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a097792fff8c2ca285d8f59615ef7d1b",
+    "url": "/toantoan22222.github.io/mypropo/index.html"
+  },
+  {
+    "revision": "912dce07dbefae296684",
+    "url": "/toantoan22222.github.io/mypropo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "502c0b121505636b5701",
+    "url": "/toantoan22222.github.io/mypropo/static/js/2.91d1e0a7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/toantoan22222.github.io/mypropo/static/js/2.91d1e0a7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "912dce07dbefae296684",
+    "url": "/toantoan22222.github.io/mypropo/static/js/main.5d58e65d.chunk.js"
+  },
+  {
+    "revision": "b7114b6cdbf3e22bcfa1",
+    "url": "/toantoan22222.github.io/mypropo/static/js/runtime-main.c9b40c4f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/toantoan22222.github.io/mypropo/static/media/logo.5d5d9eef.svg"
+  }
+]);
